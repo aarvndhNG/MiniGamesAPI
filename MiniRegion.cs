f6e29dfa3c06b0f00c4c83c10164e6b1d1874c8c
BeanPaste <1656425255@qq.com>
2022-02-25 15:31:25 +0800

    新增预制板类 游戏区域类

---
+++ MiniRegion.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGamesAPI
+{
+    public class MiniRegion
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Rectangle Area {get;set;}
+    }
+}
